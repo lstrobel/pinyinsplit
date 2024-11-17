096d80621ed2021249bca8750ad5f3117edbd4a1
lstrobel <mail@lstrobel.com>
2024-11-17 14:29:56 +0800

    Format syllable table and remove some umlaut cases

--- src/pinyin_split/pinyinsplit.py
+++ src/pinyin_split/pinyinsplit.py
@@ -4,55 +4,100 @@ from pygtrie import CharTrie
 # List of valid Pinyin syllables
 # fmt: off
 _syllables = [
-    'a', 'ai', 'an', 'ang', 'ao',
-    'ba', 'bai', 'ban', 'bang', 'bao', 'bei', 'ben', 'beng',
-    'bi', 'bian', 'biang', 'biao', 'bie', 'bin', 'bing', 'bo', 'bu',
-    'ca', 'cai', 'can', 'cang', 'cao', 'ce', 'cen', 'ceng',
-    'cha', 'chai', 'chan', 'chang', 'chao', 'che', 'chen', 'cheng',
-    'chi', 'chong', 'chou', 'chu', 'chua', 'chuai', 'chuan', 'chuang', 'chui', 'chun', 'chuo',
-    'ci', 'cong', 'cou', 'cu', 'cuan', 'cui', 'cun', 'cuo',
-    'da', 'dai', 'dan', 'dang', 'dao', 'de', 'dei', 'den', 'deng',
-    'di', 'dia', 'dian', 'diang', 'diao', 'die', 'ding', 'diu',
-    'dong', 'dou', 'du', 'duan', 'dui', 'dun', 'duo',
-    'e', 'ei', 'en', 'eng', 'er',
-    'fa', 'fan', 'fang', 'fei', 'fen', 'feng', 'fiao',
-    'fo', 'fou', 'fu', 'ga', 'gai', 'gan', 'gang', 'gao',
-    'ge', 'gei', 'gen', 'geng', 'gong', 'gou',
-    'gu', 'gua', 'guai', 'guan', 'guang', 'gui', 'gun', 'guo',
-    'ha', 'hai', 'han', 'hang', 'hao', 'he', 'hei', 'hen', 'heng',
-    'hong', 'hou', 'hu', 'hua', 'huai', 'huan', 'huang', 'hui', 'hun', 'huo',
-    'ji', 'jia', 'jian', 'jiang', 'jiao', 'jie', 'jin', 'jing', 'jiong', 'jiu', 'ju', 'juan', 'jue', 'jun',
-    'ka', 'kai', 'kan', 'kang', 'kao', 'ke', 'kei', 'ken', 'keng',
-    'kong', 'kou', 'ku', 'kua', 'kuai', 'kuan', 'kuang', 'kui', 'kun', 'kuo',
-    'la', 'lai', 'lan', 'lang', 'lao', 'le', 'lei', 'leng',
-    'li', 'lia', 'lian', 'liang', 'liao', 'lie', 'lin', 'ling', 'liu', 'long', 'lou',
-    'lu', 'luan', 'lue', 'lun', 'luo', 'lv', 'lve', 'lvn', 'lü', 'lüe', 'lün',
-    'ma', 'mai', 'man', 'mang', 'mao', 'me', 'mei', 'men', 'meng',
-    'mi', 'mian', 'miao', 'mie', 'min', 'ming', 'miu', 'mo', 'mou', 'mu',
-    'na', 'nai', 'nan', 'nang', 'nao', 'ne', 'nei', 'nen', 'neng',
-    'ni', 'nia', 'nian', 'niang', 'niao', 'nie', 'nin', 'ning', 'niu',
-    'nong', 'nou', 'nu', 'nuan', 'nue', 'nun', 'nuo', 'nv', 'nve', 'nü', 'nüe', 'ou',
-    'pa', 'pai', 'pan', 'pang', 'pao', 'pei', 'pen', 'peng',
-    'pi', 'pian', 'piao', 'pie', 'pin', 'ping', 'po', 'pou', 'pu',
-    'qi', 'qia', 'qian', 'qiang', 'qiao', 'qie',
-    'qin', 'qing', 'qiong', 'qiu', 'qu', 'quan', 'que', 'qun',
-    'ran', 'rang', 'rao', 're', 'ren', 'reng', 'ri', 'rong', 'rou',
-    'ru', 'rua', 'ruan', 'rui', 'run', 'ruo',
-    'sa', 'sai', 'san', 'sang', 'sao', 'se', 'sei', 'sen', 'seng',
-    'sha', 'shai', 'shan', 'shang', 'shao', 'she', 'shei', 'shen', 'sheng', 'shi',
-    'shong', 'shou', 'shu', 'shua', 'shuai', 'shuan', 'shuang', 'shui', 'shun', 'shuo',
-    'si', 'song', 'sou', 'su', 'suan', 'sui', 'sun', 'suo',
-    'ta', 'tai', 'tan', 'tang', 'tao', 'te', 'tei', 'teng',
-    'ti', 'tian', 'tiao', 'tie', 'ting', 'tong', 'tou',
-    'tu', 'tuan', 'tui', 'tun', 'tuo',
-    'wa', 'wai', 'wan', 'wang', 'wei', 'wen', 'weng', 'wo', 'wu',
-    'xi', 'xia', 'xian', 'xiang', 'xiao', 'xie', 'xin', 'xing', 'xiong', 'xiu', 'xu', 'xuan', 'xue', 'xun',
-    'ya', 'yai', 'yan', 'yang', 'yao', 'ye', 'yi', 'yin', 'ying',
-    'yo', 'yong', 'you', 'yu', 'yuan', 'yue', 'yun',
-    'za', 'zai', 'zan', 'zang', 'zao', 'ze', 'zei', 'zen', 'zeng',
-    'zha', 'zhai', 'zhan', 'zhang', 'zhao', 'zhe', 'zhei', 'zhen', 'zheng',
-    'zhi', 'zhong', 'zhou', 'zhu', 'zhua', 'zhuai', 'zhuan', 'zhuang', 'zhui', 'zhun', 'zhuo',
-    'zi', 'zong', 'zou', 'zu', 'zuan', 'zui', 'zun', 'zuo', 'ê'
+    'a', 'o', 'e', 'ê', 'ai', 'ei', 'ao', 'ou', 'an', 'en', 'ang', 'eng', 'er',
+    'yi', 'ya', 'yo', 'ye', 'yao', 'you', 'yan', 'yin', 'yang', 'ying',
+    'wu', 'wa', 'wo', 'wai', 'wei', 'wan', 'wen', 'wang', 'weng',
+    'yu', 'yue', 'yuan', 'yun', 'yong',
+    
+    'ba', 'bai', 'bei', 'bao', 'ban', 'ben', 'bang', 'beng',
+    'bi', 'bie', 'biao', 'bian', 'bin', 'bing',
+    'bu', 'bo',
+    
+    'pa', 'pai', 'pei', 'pao', 'pou', 'pan', 'pen', 'pang', 'peng',
+    'pi', 'pie', 'piao', 'pian', 'pin', 'ping',
+    'pu', 'po',
+    
+    'ma', 'me', 'mai', 'mei', 'mao', 'mou', 'man', 'men', 'mang', 'meng',
+    'mi', 'mie', 'miao', 'miu', 'mian', 'min', 'ming',
+    'mu', 'mo',
+    
+    'fa', 'fei', 'fou', 'fan', 'fen', 'fang', 'feng',
+    'fu', 'fo',
+    
+    'da', 'de', 'dai', 'dei', 'dao', 'dou', 'dan', 'den', 'dang', 'deng',
+    'di', 'die', 'diao', 'diu', 'dian', 'din', 'ding',
+    'du', 'duo', 'dui', 'duan', 'dun', 'dong',
+    
+    'ta', 'te', 'tai', 'tao', 'tou', 'tan', 'tang', 'teng',
+    'ti', 'tie', 'tiao', 'tian', 'ting',
+    'tu', 'tuo', 'tui', 'tuan', 'tun', 'tong',
+    
+    'na', 'ne', 'nai', 'nei', 'nao', 'nou', 'nan', 'nen', 'nang', 'neng',
+    'ni', 'nie', 'niao', 'niu', 'nian', 'nin', 'niang', 'ning',
+    'nu', 'nuo', 'nuan', 'nun', 'nong',
+    'nü', 'nüe',
+    
+    'la', 'lo', 'le', 'lai', 'lei', 'lao', 'lou', 'lan', 'lang', 'leng',
+    'li', 'lie', 'liao', 'liu', 'lian', 'lin', 'liang', 'ling',
+    'lu', 'luo', 'luan', 'lun', 'long',
+    'lü', 'lüe',
+    
+    'ga', 'ge', 'gai', 'gei', 'gao', 'gou', 'gan', 'gen', 'gang', 'geng',
+    'gu', 'gua', 'guo', 'guai', 'gui', 'guan', 'gun', 'guang', 'gong',
+    
+    'ka', 'ke', 'kai', 'kao', 'kou', 'kan', 'ken', 'kang', 'keng',
+    'ku', 'kua', 'kuo', 'kuai', 'kui', 'kuan', 'kun', 'kuang', 'kong',
+    
+    'ha', 'he', 'hai', 'hei', 'hao', 'hou', 'han', 'hen', 'hang', 'heng',
+    'hu', 'hua', 'huo', 'huai', 'hui', 'huan', 'hun', 'huang', 'hong',
+    
+    'ji', 'jia', 'jie', 'jiao', 'jiu', 'jian', 'jin', 'jiang', 'jing',
+    'ju', 'jue', 'juan', 'jun', 'jiong',
+    
+    'qi', 'qia', 'qie', 'qiao', 'qiu', 'qian', 'qin', 'qiang', 'qing',
+    'qu', 'que', 'quan', 'qun', 'qiong',
+
+    'xi', 'xia', 'xie', 'xiao', 'xiu', 'xian', 'xin', 'xiang', 'xing',
+    'xu', 'xue', 'xuan', 'xun', 'xiong',
+    
+    'zhi', 'zha', 'zhe', 'zhai', 'zhao', 'zhou', 'zhan', 'zhen', 'zhang', 'zheng',
+    'zhu', 'zhua', 'zhuo', 'zhuai', 'zhui', 'zhuan', 'zhun', 'zhuang', 'zhong',
+
+    'chi', 'cha', 'che', 'chai', 'chao', 'chou', 'chan', 'chen', 'chang', 'cheng',
+    'chu', 'chua', 'chuo', 'chuai', 'chui', 'chuan', 'chun', 'chuang', 'chong',
+
+    'shi', 'sha', 'she', 'shai', 'shei', 'shao', 'shou', 'shan', 'shen', 'shang', 'sheng',
+    'shu', 'shua', 'shuo', 'shuai', 'shui', 'shuan', 'shun', 'shuang',
+
+    'ri', 're', 'rao', 'rou', 'ran', 'ren', 'rang', 'reng',
+    'ru', 'ruo', 'rui', 'ruan', 'run', 'rong',
+
+    'zi', 'za', 'ze', 'zai', 'zei', 'zao', 'zou', 'zan', 'zen', 'zang', 'zeng',
+    'zu', 'zuo', 'zui', 'zuan', 'zun', 'zong',
+
+    'ci', 'ca', 'ce', 'cai', 'cao', 'cou', 'can', 'cen', 'cang', 'ceng',
+    'cu', 'cuo', 'cui', 'cuan', 'cun', 'cong',
+
+    'si', 'sa', 'se', 'sai', 'sao', 'sou', 'san', 'sen', 'sang', 'seng',
+    'su', 'suo', 'sui', 'suan', 'sun', 'song',
+]
+
+_non_standard_syllables = [
+    'yai', 'ong', 
+    'biang', 
+    'pia', 'pun',
+    'fai', 'fiao',
+    'dia', 'diang', 'duang',
+    'tei', 
+    'nia', 'nui',
+    'len', 'lia',
+    'lüan', 'lün',
+    'gin', 'ging', 
+    'kei', 'kiu', 'kiang',
+    'zhei',
+    'rua',
+    'cei',
+    'sei'
 ]
 # fmt: on
 
@@ -102,7 +147,7 @@ def split(phrase: str) -> list[list[str]]:
         # Find all valid pinyin prefixes
         prefix_matches = _trie.prefixes(current_lower)
 
-        for prefix, length in prefix_matches:
+        for _, length in prefix_matches:
             # Extract the matched prefix and remaining text
             matched_syllable = current[:length].lower()
             remaining_text = current[length:]
